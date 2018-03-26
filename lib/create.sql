4bc3a25a5e46ae525dcc26e38843bdfd754ea285
Kaushal Patel <kaushalpatel778@gmail.com>
2018-03-26 03:07:22 +0000

    automatically backed up by learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,9 +1,10 @@
 CREATE TABLE bears(
+  id INTEGER PRIMARY KEY,
   name text,
   age integer,
   gender char(1),
   color text,
   temperament text,
-  alive boolean,
-  id INTEGER PRIMARY KEY
+  alive boolean
+
 );
